32daea374efc7c33a841a850009bccc668577bc4
NeilRow56 <neilrowland56@gmail.com>
2024-01-02 07:45:49 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  // eslint: {
+  // //   Warning: This allows production builds to successfully complete even if
+  // //   your project has ESLint errors.
+  //   ignoreDuringBuilds: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
